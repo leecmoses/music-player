69a5f6cb4252a8011fe2a4784b4b8bc48454addf
moses <leecmoses@gmail.com>
2022-06-15 11:41:45 -0400

    Logic for rest of music player controls

--- script.js
+++ script.js
@@ -1,9 +1,36 @@
 'use strict';
 
+const image = document.querySelector('img');
+const title = document.querySelector('#title');
+const artist = document.querySelector('#artist');
 const music = document.querySelector('audio');
 const prevBtn = document.querySelector('#prev');
 const playBtn = document.querySelector('#play');
 const nextBtn = document.querySelector('#next');
+
+// Music
+const songs = [
+    {
+        name: 'jacinto-1',
+        displayName: 'Electric Chill Machine',
+        artist: 'Jacinto Design',
+    },
+    {
+        name: 'jacinto-2',
+        displayName: 'Seven Nation Army (Remix)',
+        artist: 'Jacinto Design',
+    },
+    {
+        name: 'jacinto-3',
+        displayName: 'Goodnight, Disco Queen',
+        artist: 'Jacinto Design',
+    },
+    {
+        name: 'metric-1',
+        displayName: 'Front Row (Remix)',
+        artist: 'Metric/Jacinto Design',
+    },
+];
 
 // Check if Playing
 let isPlaying = false;
@@ -26,3 +53,37 @@ function pauseSong() {
 
 // Play or Pause Event Listener
 playBtn.addEventListener('click', () => (isPlaying ? pauseSong() : playSong()));
+
+// Update DOM
+function loadSong(song) {
+    title.textContent = song.displayName;
+    artist.textContent = song.artist;
+    music.src = `music/${song.name}.mp3`;
+    image.src = `img/${song.name}.jpg`;
+}
+
+// Current Song
+let songIndex = 0;
+
+// Previous Song
+const prevSong = () => {
+    songIndex--;
+    songIndex < 0 && (songIndex = songs.length - 1);
+    loadSong(songs[songIndex]);
+    playSong();
+}
+
+// Next Song
+const nextSong = () => {
+    songIndex++;
+    songIndex > songs.length - 1 && (songIndex = 0);
+    loadSong(songs[songIndex]);
+    playSong();
+}
+
+// On Load - Select First Song
+loadSong(songs[songIndex]);
+
+// Event Listeners
+prevBtn.addEventListener('click', prevSong);
+nextBtn.addEventListener('click', nextSong);
